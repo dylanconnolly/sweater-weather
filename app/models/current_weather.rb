8f3d2acc9ba9c911ebabc4b4d652ac39415805d8
Dylan Connolly <dylanconnolly145@gmail.com>
2020-03-02 00:53:15 -0700

    Create poros to hold information for each of the three boxes on forecast view

---
+++ app/models/current_weather.rb
@@ -0,0 +1,14 @@
+class CurrentWeather
+
+  def initialize(darksky_api_data, geocode_api_data)
+    @time = Time.at(darksky_api_data[:currently][:time])
+    @summary = darksky_api_data[:currently][:summary]
+    @icon = darksky_api_data[:currently][:icon]
+    @temperature = darksky_api_data[:currently][:temperature]
+    @high_temp = darksky_api_data[:daily][:data].first[:temperatureHigh]
+    @low_temp = darksky_api_data[:daily][:data].first[:temperatureLow]
+    @city = geocode_api_data[:results].first[:address_components].first[:short_name]
+    @state = geocode_api_data[:results].first[:address_components][-2][:short_name]
+    @country = geocode_api_data[:results].first[:address_components][-1][:long_name]
+  end
+end
---
+++ app/models/weather_details.rb
@@ -0,0 +1,15 @@
+class WeatherDetails
+
+  def initialize(darksky_api_data)
+    @today_desc = darksky_api_data[:daily][:summary]
+    # @tonight_desc = darksky_api_data[:hourly][time_at_9pm.to_s]
+    @feels_like = darksky_api_data[:currently][:apparentTemperature]
+    @humidity = darksky_api_data[:currently][:humidity]
+    @visibility = darksky_api_data[:currently][:visibility]
+    @uv_index = darksky_api_data[:currently][:uvIndex]
+  end
+
+  def time_at_9pm
+    Time.now.change(hour: 21).to_i
+  end
+end
