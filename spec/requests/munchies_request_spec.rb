21f2ab02fe40a2085653b976c7e1aff547ab5377
Dylan Connolly <dylanconnolly145@gmail.com>
2020-03-02 10:43:36 -0700

    Add assertions to test for expected output

--- spec/requests/munchies_request_spec.rb
+++ spec/requests/munchies_request_spec.rb
@@ -6,5 +6,13 @@ describe 'munchies endpoint' do
       get '/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese'
     end
 
+    expect(response).to be_successful
+
+    parsed = JSON.parse(response.body, symbolize_names: true)[:data]
+    
+    expect(parsed[:type]).to eq("munchie")
+    expect(parsed[:attributes][:end_location]).to eq("Pueblo, CO")
+    expect(parsed[:attributes][:travel_time]).to eq("1 hour 48 mins")
+    expect(parsed[:attributes][:restaurant].class).to eq(Hash)
   end
 end
