63b481168a13729b7279c87e84b2c4f41ba251fe
Dylan Connolly <dylanconnolly145@gmail.com>
2020-03-04 08:32:36 -0700

    Create roadtrip spec and re generate vcr cassettes

--- spec/requests/roadtrip_request_spec.rb
+++ spec/requests/roadtrip_request_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 describe 'when a post request is made to /roadtrip' do
-  it 'a road trip json is returned if a valid api key was provided in the request' do
+  it 'a road trip json is returned if a valid api key was provided in the request', :vcr do
 
     user = User.create(email: "whatever@example.com", password: "password", api_key: "supersafeapikey")
+
+    allow(Time).to receive(:now).and_return(Time.parse('Wed, 04 Mar 2020 15:17:38 GMT'))
 
     request_body = {
                       "origin": "Denver,CO",
@@ -13,6 +15,12 @@ describe 'when a post request is made to /roadtrip' do
 
     post '/api/v1/road_trip', params: request_body
 
-    
+    expect(response).to be_successful
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed[:data][:attributes][:origin]).to eq("Denver, CO, USA")
+    expect(parsed[:data][:attributes][:destination]).to eq("Pueblo, CO, USA")
+    expect(parsed[:data][:attributes][:travel_time]).to eq("1 hour 48 mins")
   end
 end
