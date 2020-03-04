6794a4d0dd02cbfc68ccb93baf42bb21803c2345
Dylan Connolly <dylanconnolly145@gmail.com>
2020-03-04 08:50:22 -0700

    Add tonight description to weather details which gets the future forecast for the arrival location

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -12,11 +12,18 @@ class Api::V1::ForecastController < ApplicationController
 
     forecast_data = DarkskyService.new.get_forecast(coords[:lat], coords[:lng])
 
+    future_forecast_data = DarkskyService.new.get_future_forecast(coords[:lat], coords[:lng], time_at_9pm)
 
     current_weather = CurrentWeather.new(forecast_data, geocode_data)
-    weather_details = WeatherDetails.new(forecast_data)
+    weather_details = WeatherDetails.new(forecast_data, future_forecast_data)
     forecast_info = ForecastInfo.new(forecast_data)
-
+    
     render json: ForecastSerializer.new([current_weather, weather_details, forecast_info])
+  end
+
+  private
+
+  def time_at_9pm
+      Time.now.change(hour: 21).to_i
   end
 end
--- app/serializers/weather_details_serializer.rb
+++ app/serializers/weather_details_serializer.rb
@@ -2,5 +2,5 @@ class WeatherDetailsSerializer
   include FastJsonapi::ObjectSerializer
     set_id {"null"}
 
-    attributes :today_desc, :feels_like, :humidity, :visibility, :uv_index
+    attributes :today_desc, :tonight_desc, :feels_like, :humidity, :visibility, :uv_index
 end
