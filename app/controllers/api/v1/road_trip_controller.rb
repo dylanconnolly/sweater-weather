c67ab42ccf2d10ab211985c97e62c9948eb0cd1d
Dylan Connolly <dylanconnolly145@gmail.com>
2020-03-04 08:32:52 -0700

    Create road trip controller with before action to verify api key

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -2,7 +2,15 @@ class Api::V1::RoadTripController < ApplicationController
   before_action :valid_key?
 
   def create
-    require "pry"; binding.pry
+    directions_api_data = GoogleMapsService.new.get_directions(params[:origin], params[:destination])
+
+    directions = Directions.new(directions_api_data)
+
+    forecast_data = DarkskyService.new.get_future_forecast(directions.destination_latitude, directions.destination_longitude, arrival_time(directions))
+
+    roadtrip = RoadTrip.new(directions, forecast_data)
+
+    render json: RoadTripSerializer.new(roadtrip)
   end
 
   private
@@ -13,5 +21,9 @@ class Api::V1::RoadTripController < ApplicationController
 
   def road_trip_params
     params.permit(:origin, :destination)
+  end
+
+  def arrival_time(directions)
+    (Time.now + directions.travel_time_seconds).to_i
   end
 end
